38388028635aac2ae40cf06bcefc59f720902987
kabutz <heinz@javaspecialists.eu>
2018-05-28 17:12:22 +0300

    First benchmark - converting an int to a String

---
+++ jdk-8/src/main/java/eu/javaspecialists/perf/string/IntToStringBenchmark.java
@@ -0,0 +1,32 @@
+package eu.javaspecialists.perf.string;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.*;
+
+import java.lang.invoke.*;
+import java.util.concurrent.*;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Fork(3)
+@Warmup(iterations = 5)
+@Measurement(iterations = 10)
+@State(Scope.Benchmark)
+public class IntToStringBenchmark {
+  private int value;
+
+  @Setup
+  public void setup() {
+    value = ThreadLocalRandom.current().nextInt(10, 100);
+  }
+
+  @Benchmark
+  public void lazyIntToString(Blackhole bh) {
+    bh.consume("" + value);
+  }
+
+  @Benchmark
+  public void studiousIntToString1(Blackhole bh) {
+    bh.consume(Integer.toString(value));
+  }
+}
