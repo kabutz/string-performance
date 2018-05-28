25acbd287d9a627e5dc03703419aadd4fb9152dd
kabutz <heinz@javaspecialists.eu>
2018-05-28 17:12:22 +0300

    First benchmark - converting an int to a String

---
+++ jdk-9/src/main/java/eu/javaspecialists/perf/string/StringConcatFactoryTest.java
@@ -0,0 +1,9 @@
+package eu.javaspecialists.perf.string;
+
+import java.lang.invoke.*;
+
+public class StringConcatFactoryTest {
+  public static void main(String... args) {
+//    StringConcatFactory.makeConcat()
+  }
+}
