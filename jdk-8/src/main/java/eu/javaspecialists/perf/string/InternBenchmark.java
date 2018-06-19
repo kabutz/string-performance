96bfa13ae985b916aaa68208bf2a4f30fbfacf1c
kabutz <heinz@javaspecialists.eu>
2018-06-19 07:26:44 +0200

    intern bench

---
+++ jdk-8/src/main/java/eu/javaspecialists/perf/string/InternBenchmark.java
@@ -0,0 +1,40 @@
+package eu.javaspecialists.perf.string;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.*;
+
+import java.util.concurrent.*;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Fork(3)
+@Warmup(iterations = 10)
+@Measurement(iterations = 20)
+@State(Scope.Benchmark)
+public class InternBenchmark {
+  @Param({"100", "10000", "100000", "1000000", "10000000"})
+  private int limit;
+
+  private int value;
+
+  @Setup
+  public void setup() {
+    value = 100;
+  }
+
+  @Benchmark
+  public void creatingStrings(Blackhole bh) {
+    bh.consume(nextValue());
+  }
+
+  @Benchmark
+  public void interningString(Blackhole bh) {
+    bh.consume(nextValue().intern());
+  }
+
+  private String nextValue() {
+    value++;
+    if (value > limit) value = 0;
+    return Integer.toHexString(value);
+  }
+}
