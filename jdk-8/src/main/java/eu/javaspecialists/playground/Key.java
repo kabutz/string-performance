c11fde106b0a584d232306f16f0bf2f51f06551e
kabutz <HEz8TnXp4HSR5kqL>
2018-10-05 17:12:02 +0300

    Shows why caching hash codes in key objects does not really help all that much

---
+++ jdk-8/src/main/java/eu/javaspecialists/playground/Key.java
@@ -0,0 +1,23 @@
+package eu.javaspecialists.playground;
+
+public class Key {
+  private final int id;
+
+  public Key(int id) {
+    this.id = id;
+  }
+
+  public int hashCode() {
+    try {
+      Thread.sleep(100); // now THIS is slow
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+    }
+    return id;
+  }
+
+  public boolean equals(Object obj) {
+    if (!(obj instanceof Key)) return false;
+    return id == ((Key)obj).id;
+  }
+}
