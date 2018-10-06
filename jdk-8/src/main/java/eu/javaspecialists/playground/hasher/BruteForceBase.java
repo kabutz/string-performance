512da5b5767d97ba28b9574db971bc36f0c638f9
kabutz <HEz8TnXp4HSR5kqL>
2018-10-06 10:07:47 +0300

    Three calculations for finding Strings with hashCode==0

---
+++ jdk-8/src/main/java/eu/javaspecialists/playground/hasher/BruteForceBase.java
@@ -0,0 +1,29 @@
+package eu.javaspecialists.playground.hasher;
+
+public abstract class BruteForceBase {
+  protected static final byte[] alphabet = new byte[26 * 2];
+  protected static long number;
+  private final static boolean DEBUG = true;
+  protected static long time;
+
+  static {
+    for (int i = 0; i < 26; i++) {
+      alphabet[i] = (byte) ('A' + i);
+      alphabet[i + 26] = (byte) ('a' + i);
+    }
+    System.out.println("alphabet = " + new String(alphabet));
+    time = System.nanoTime();
+  }
+
+  protected static void checkProgress() {
+    if (DEBUG) {
+      if (++number % 10_000_000_000L == 0) {
+        System.out.println("number = " + number);
+        time = System.nanoTime() - time;
+        System.out.println("start = " + time / 1_000_000 + "ms");
+        time = System.nanoTime();
+        System.exit(1);
+      }
+    }
+  }
+}
---
+++ jdk-8/src/main/java/eu/javaspecialists/playground/hasher/BruteForceRecursive2.java
@@ -0,0 +1,30 @@
+package eu.javaspecialists.playground.hasher;
+
+import java.util.*;
+
+/*
+number = 10000000000
+start = 40056ms
+ */
+public class BruteForceRecursive2 extends BruteForceBase {
+  public static void main(String... args) {
+    byte[] is = new byte[7];
+    Arrays.fill(is, (byte) '!');
+    check(0, 0, is);
+  }
+
+  private static void check(int depth, int h, byte[] is) {
+    if (depth == 7) {
+      if (h == 0) {
+        System.out.println(new String(is));
+      }
+      checkProgress();
+      return;
+    }
+
+    for (byte i : alphabet) {
+      is[depth] = i;
+      check(depth + 1, h * 31 + i, is);
+    }
+  }
+}
