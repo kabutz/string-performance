ce2ccf89e31774c6b7e300c0092585fdb744df9a
Dmitry Vyazelenko <dmitry.vyazelenko@canoo.com>
2018-10-09 13:42:52 +0200

    Move all sources into a single sourceSet

--- jdk-11/src/main/java/eu/javaspecialists/perf/string/StringJava9AppendBenchmarkUsingMH_SB_SIZED_EXACT.java
+++
@@ -1,7 +0,0 @@
-package eu.javaspecialists.perf.string;
-
-import org.openjdk.jmh.annotations.*;
-
-@Fork(value = 3, jvmArgsAppend = {"-Djava.lang.invoke.stringConcat=MH_SB_SIZED"})//, "-Djava.lang.invoke.stringConcat.debug=true"})
-public class StringJava9AppendBenchmarkUsingMH_SB_SIZED_EXACT extends AbstractStringAppendBenchmark {
-}
