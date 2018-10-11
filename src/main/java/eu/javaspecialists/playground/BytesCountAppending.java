14385688ef55e9a42221e97781e9d0b78f18c4a2
kabutz <HEz8TnXp4HSR5kqL>
2018-10-11 13:47:56 +0300

    updated byte counting class to warmup hotspot before measuring size allocation

--- src/main/java/eu/javaspecialists/playground/BytesCountAppending.java
+++ src/main/java/eu/javaspecialists/playground/BytesCountAppending.java
@@ -22,25 +22,45 @@ Total: 192
 public class BytesCountAppending {
   public static void main(String... args) {
     ByteWatcherSingleThread bw = new ByteWatcherSingleThread();
-    concat("id, insert_time", "history", "1539203113091");
-    sb("id, insert_time", "history", "1539203113091");
-    sb_sized("id, insert_time", "history", "1539203113091");
+    for (int i = 0; i < 1_000_000; i++) {
+      concat("id, insert_time", "history", "1539203113091");
+      sb("id, insert_time", "history", "1539203113091");
+      sb_sized("id, insert_time", "history", "1539203113091");
+      concat("id, insert_time", "history", 1539203113091L);
+      sb("id, insert_time", "history", 1539203113091L);
+      sb_sized("id, insert_time", "history", 1539203113091L);
+    }
 
     bw.reset();
     concat("id, insert_time", "history", "1539203113091");
-    long concat_bytes = bw.calculateAllocations();
+    long concat_bytes_strings = bw.calculateAllocations();
 
     bw.reset();
     sb("id, insert_time", "history", "1539203113091");
-    long sb_bytes = bw.calculateAllocations();
+    long sb_bytes_strings = bw.calculateAllocations();
 
     bw.reset();
     sb_sized("id, insert_time", "history", "1539203113091");
-    long sb_sized_bytes = bw.calculateAllocations();
+    long sb_sized_bytes_strings = bw.calculateAllocations();
 
-    System.out.println("concat_bytes = " + concat_bytes);
-    System.out.println("sb_bytes = " + sb_bytes);
-    System.out.println("sb_sized_bytes = " + sb_sized_bytes);
+    bw.reset();
+    concat("id, insert_time", "history", 1539203113091L);
+    long concat_bytes_mixed = bw.calculateAllocations();
+
+    bw.reset();
+    sb("id, insert_time", "history", 1539203113091L);
+    long sb_bytes_mixed = bw.calculateAllocations();
+
+    bw.reset();
+    sb_sized("id, insert_time", "history", 1539203113091L);
+    long sb_sized_bytes_mixed = bw.calculateAllocations();
+
+    System.out.println("concat_bytes_strings = " + concat_bytes_strings);
+    System.out.println("sb_bytes_strings = " + sb_bytes_strings);
+    System.out.println("sb_sized_bytes_strings = " + sb_sized_bytes_strings);
+    System.out.println("concat_bytes_mixed = " + concat_bytes_mixed);
+    System.out.println("sb_bytes_mixed = " + sb_bytes_mixed);
+    System.out.println("sb_sized_bytes_mixed = " + sb_sized_bytes_mixed);
   }
 
   public static String concat(String columns, String table, String time) {
@@ -53,6 +73,19 @@ public class BytesCountAppending {
   }
 
   public static String sb_sized(String columns, String table, String time) {
+    return new StringBuilder(39 + columns.length() + table.length() + 19)
+        .append("SELECT ").append(columns).append(" FROM ").append(table).append(" WHERE last_update_time > ").append(time).toString();
+  }
+  public static String concat(String columns, String table, long time) {
+    return "SELECT " + columns + " FROM " + table + " WHERE last_update_time > " + time;
+  }
+
+  public static String sb(String columns, String table, long time) {
+    return new StringBuilder()
+        .append("SELECT ").append(columns).append(" FROM ").append(table).append(" WHERE last_update_time > ").append(time).toString();
+  }
+
+  public static String sb_sized(String columns, String table, long time) {
     return new StringBuilder(39 + columns.length() + table.length() + 19)
         .append("SELECT ").append(columns).append(" FROM ").append(table).append(" WHERE last_update_time > ").append(time).toString();
   }
