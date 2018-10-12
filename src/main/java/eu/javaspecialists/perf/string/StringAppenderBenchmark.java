bc548121864bbcf85bfbc7b23093ac97efa07d09
kabutz <HEz8TnXp4HSR5kqL>
2018-10-12 12:13:34 +0300

    Replaced SQL example with HTML

--- src/main/java/eu/javaspecialists/perf/string/StringAppenderBenchmark.java
+++ src/main/java/eu/javaspecialists/perf/string/StringAppenderBenchmark.java
@@ -8,27 +8,43 @@ import java.util.concurrent.TimeUnit;
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @State(Scope.Benchmark)
 public class StringAppenderBenchmark {
-  @Param("id, insert_time")
-  private String columns;
-  @Param("history")
-  private String table;
+  @Param("What is the fastest way to append Strings?")
+  private String title;
   @Param("1539203113091")
-  private long time;
+  private long id1;
+  @Param("Plain old +")
+  private String optiontxt1;
+  @Param("3472387872424")
+  private long id2;
+  @Param("StringBuilder.append()")
+  private String optiontxt2;
 
   @Benchmark
   public String concat() {
-    return "SELECT " + columns + " FROM " + table + " WHERE last_update_time > " + time;
+    return "<h1>" + title + "</h1>" +
+        "<ul>" +
+        "<li><b>" + id1 + "</b> " + optiontxt1 + "</li>" +
+        "<li><b>" + id2 + "</b> " + optiontxt2 + "</li>" +
+        "</ul>";
   }
 
   @Benchmark
   public String sb() {
-    return new StringBuilder()
-        .append("SELECT ").append(columns).append(" FROM ").append(table).append(" WHERE last_update_time > ").append(time).toString();
+    return new StringBuilder().append("<h1>").append(title)
+        .append("</h1>").append("<ul>").append("<li><b>")
+        .append(id1).append("</b> ").append(optiontxt1)
+        .append("</li>").append("<li><b>").append(id2)
+        .append("</b> ").append(optiontxt2).append("</li>")
+        .append("</ul>").toString();
   }
 
   @Benchmark
   public String sb_sized() {
-    return new StringBuilder(39 + columns.length() + table.length() + 19)
-        .append("SELECT ").append(columns).append(" FROM ").append(table).append(" WHERE last_update_time > ").append(time).toString();
+    return new StringBuilder(52 + title.length() + 19 + optiontxt1.length() + 19 + optiontxt2.length())
+        .append("<h1>").append(title).append("</h1>")
+        .append("<ul>").append("<li><b>").append(id1).append("</b> ")
+        .append(optiontxt1).append("</li>")
+        .append("<li><b>").append(id2).append("</b> ")
+        .append(optiontxt2).append("</li>").append("</ul>").toString();
   }
 }
