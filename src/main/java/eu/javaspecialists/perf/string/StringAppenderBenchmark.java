660c43dc05d9c1e709d1d164e9f39576c2fa21bd
kabutz <HEz8TnXp4HSR5kqL>
2018-10-12 12:26:14 +0300

    Undoing earlier attempt to replace SQL with HTML

--- src/main/java/eu/javaspecialists/perf/string/StringAppenderBenchmark.java
+++ src/main/java/eu/javaspecialists/perf/string/StringAppenderBenchmark.java
@@ -8,43 +8,27 @@ import java.util.concurrent.TimeUnit;
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @State(Scope.Benchmark)
 public class StringAppenderBenchmark {
-  @Param("What is the fastest way to append Strings?")
-  private String title;
+  @Param("id, insert_time")
+  private String columns;
+  @Param("history")
+  private String table;
   @Param("1539203113091")
-  private long id1;
-  @Param("Plain old +")
-  private String optiontxt1;
-  @Param("3472387872424")
-  private long id2;
-  @Param("StringBuilder.append()")
-  private String optiontxt2;
+  private long time;
 
   @Benchmark
   public String concat() {
-    return "<h1>" + title + "</h1>" +
-        "<ul>" +
-        "<li><b>" + id1 + "</b> " + optiontxt1 + "</li>" +
-        "<li><b>" + id2 + "</b> " + optiontxt2 + "</li>" +
-        "</ul>";
+    return "SELECT " + columns + " FROM " + table + " WHERE last_update_time > " + time;
   }
 
   @Benchmark
   public String sb() {
-    return new StringBuilder().append("<h1>").append(title)
-        .append("</h1>").append("<ul>").append("<li><b>")
-        .append(id1).append("</b> ").append(optiontxt1)
-        .append("</li>").append("<li><b>").append(id2)
-        .append("</b> ").append(optiontxt2).append("</li>")
-        .append("</ul>").toString();
+    return new StringBuilder()
+        .append("SELECT ").append(columns).append(" FROM ").append(table).append(" WHERE last_update_time > ").append(time).toString();
   }
 
   @Benchmark
   public String sb_sized() {
-    return new StringBuilder(52 + title.length() + 19 + optiontxt1.length() + 19 + optiontxt2.length())
-        .append("<h1>").append(title).append("</h1>")
-        .append("<ul>").append("<li><b>").append(id1).append("</b> ")
-        .append(optiontxt1).append("</li>")
-        .append("<li><b>").append(id2).append("</b> ")
-        .append(optiontxt2).append("</li>").append("</ul>").toString();
+    return new StringBuilder(39 + columns.length() + table.length() + 19)
+        .append("SELECT ").append(columns).append(" FROM ").append(table).append(" WHERE last_update_time > ").append(time).toString();
   }
 }
