39abe5efca5044e16ad4bda0f05a8d2d1548fa08
Dmitry Vyazelenko <dmitry.vyazelenko@canoo.com>
2018-10-12 10:55:35 +0200

    Set warmup/iterations/forks explicitly
    (cherry picked from commit 544bd3bb5bc1e1b9114237d405b84df6af522aab)

--- src/main/java/eu/javaspecialists/perf/string/StringAppenderBenchmark.java
+++ src/main/java/eu/javaspecialists/perf/string/StringAppenderBenchmark.java
@@ -4,6 +4,9 @@ import org.openjdk.jmh.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
+@Fork(3)
+@Warmup(iterations = 5, time = 5)
+@Measurement(iterations = 10, time = 2)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @Fork(3)
