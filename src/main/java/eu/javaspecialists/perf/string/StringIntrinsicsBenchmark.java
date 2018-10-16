f0126f811a9f571fdbcac3e2b95f0cb15f6fe582
kabutz <HEz8TnXp4HSR5kqL>
2018-10-16 10:28:30 +0300

    Testing different length of Strings and also allowing strings to be deduplicated

--- src/main/java/eu/javaspecialists/perf/string/StringIntrinsicsBenchmark.java
+++ src/main/java/eu/javaspecialists/perf/string/StringIntrinsicsBenchmark.java
@@ -2,7 +2,8 @@ package eu.javaspecialists.perf.string;
 
 import org.openjdk.jmh.annotations.*;
 
-import java.util.concurrent.TimeUnit;
+import java.lang.reflect.*;
+import java.util.concurrent.*;
 
 @Fork(3)
 @Warmup(iterations = 5, time = 5)
@@ -11,17 +12,45 @@ import java.util.concurrent.TimeUnit;
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @State(Scope.Benchmark)
 public class StringIntrinsicsBenchmark {
-  @Param("The quick brown fox jumps over the lazy dog")
+  @Param({"10", "1000", "100000"})
+  private int length;
+
   private String s1;
-  @Param("The quick brown fox jumps over the lazy dog")
   private String s2;
   private byte[] s1Bytes;
   private byte[] s2Bytes;
 
   @Setup
-  public void setup() {
+  public void setup() throws NoSuchFieldException, IllegalAccessException, InterruptedException {
+    s1 = createString(length);
+    s2 = createString(length);
     s1Bytes = asLatin1ByteArray(s1);
     s2Bytes = asLatin1ByteArray(s2);
+
+    Field valueField = String.class.getDeclaredField("value");
+    valueField.setAccessible(true);
+    boolean deduplicated = false;
+    System.out.print("Check for deduplication ..");
+    for (int i = 0; i < 10 && !deduplicated; i++) {
+      System.out.print('.');
+      System.gc();
+      Thread.sleep(100);
+      deduplicated = (valueField.get(s1) == valueField.get(s2));
+    }
+    if (deduplicated) {
+      System.out.println(" deduplicated!");
+    } else {
+      System.out.println(" not deduplicated.");
+    }
+  }
+
+  private String createString(int length) {
+    StringBuilder sb = new StringBuilder();
+    while (sb.length() < length) {
+      sb.append("The quick brown fox jumps over the lazy dog.  ");
+    }
+    sb.setLength(length);
+    return sb.toString();
   }
 
   private byte[] asLatin1ByteArray(String value) {
