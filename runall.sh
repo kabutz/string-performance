64f52462021893a485cd95460247adc1109e5e3a
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2021-10-20 21:04:24 +0200

    updated results for playground runs

---
+++ runall.sh
@@ -0,0 +1,13 @@
+DATE=`date "+%Y-%m-%d-%M-%S"`
+
+for i in 6 7 8; do 
+	echo $i; 
+	export JAVA_HOME=/opt/jdk-$i
+	/opt/jdk-$i/bin/java -showversion -jar build/libs/string-performance-benchmarks-jdk-$i.jar -jvmArgs '-Xmx8g -XX:-TieredCompilation -XX:+UseParallelGC' > java-jdk$i-$DATE.out 
+done
+ 
+for i in 11 17; do 
+	echo $i; 
+	export JAVA_HOME=/opt/jdk-$i
+	/opt/jdk-$i/bin/java -showversion --add-opens java.base/java.io=ALL-UNNAMED -jar build/libs/string-performance-benchmarks-jdk-$i.jar -jvmArgs '-Xmx8g -XX:-TieredCompilation -XX:+UseParallelGC' > java-jdk$i-$DATE.out 
+done
